34e5a80951a3d85a351dc4e947ce0b94459b7bbb
Rosannavdt <68822156+Rosannavdt@users.noreply.github.com>
2020-09-11 13:26:18 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <a
-          href="https://github.com/Rosannavdt/final-project-weather-app-react"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Open-sourced code
-        </a>
-        , by{" "}
-        <a
-          href="https://www.linkedin.com/in/rosannavandentillaar/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Rosanna van den Tillaar
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/Rosannavdt/final-project-weather-app-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-sourced code
+          </a>
+          , by{" "}
+          <a
+            href="https://www.linkedin.com/in/rosannavandentillaar/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Rosanna van den Tillaar
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
