8db8ea954b0abd3a514157abf30406dc357f1d0a
Rosannavdt <68822156+Rosannavdt@users.noreply.github.com>
2020-09-11 12:45:48 +0200

    Added Github Open-source Code

--- src/App.js
+++ src/App.js
@@ -2,5 +2,25 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/Rosannavdt/final-project-weather-app-react"
+          target="_blank"
+        >
+          Open-sourced code
+        </a>
+        , by{" "}
+        <a
+          href="https://www.linkedin.com/in/rosannavandentillaar/"
+          target="_blank"
+        >
+          {" "}
+          Rosanna van den Tillaar{" "}
+        </a>
+      </footer>
+    </div>
+  );
 }
