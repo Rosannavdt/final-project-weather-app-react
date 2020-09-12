6565807a33dcb5935f309b1e27e6838f89884c6a
Rosannavdt <68822156+Rosannavdt@users.noreply.github.com>
2020-09-12 12:20:18 +0200

    Added Forecast

---
+++ src/WeatherForecast.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./WeatherForecast.css";
+import WeatherForecastPreview from "./WeatherForecastPreview";
+
+export default function WeatherForecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
+  function handleForecastResponse(response) {
+    setForecast(response.data);
+    setLoaded(true);
+  }
+
+  if (loaded && props.city === forecast.city.name) {
+    return (
+      <div className="WeatherForecast row">
+        <WeatherForecastPreview data={forecast.list[0]} />
+        <WeatherForecastPreview data={forecast.list[1]} />
+        <WeatherForecastPreview data={forecast.list[2]} />
+        <WeatherForecastPreview data={forecast.list[3]} />
+        <WeatherForecastPreview data={forecast.list[4]} />
+        <WeatherForecastPreview data={forecast.list[5]} />
+      </div>
+    );
+  } else {
+    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=dd7b8dcf6824a84cbddcbe0b7bc1a661&units=metric`;
+    axios.get(url).then(handleForecastResponse);
+    return null;
+  }
+}
